29ee945a218f2dfd1b217f705e14f1749c5b3d1f
Martin Lienhart <martin.lienhart@outlook.com>
2022-04-19 21:23:56 +0200

    hamming-distance

---
+++ hamming-distance.cs
@@ -0,0 +1,29 @@
+public class Solution
+{
+    public int HammingDistance(int x, int y)
+    {
+        string a = Convert.ToString(x, 2);
+        string b = Convert.ToString(y, 2);
+        int c = 0;
+        char d = '0';
+
+        if (a.Length < b.Length)
+        {
+            a = a.PadLeft(b.Length, d);
+        }
+        else
+        {
+            b = b.PadLeft(a.Length, d);
+        }
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+            {
+                c++;
+            }
+        }
+
+        return c;
+    }
+}
